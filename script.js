7c83a18396facf9316f24d8238ac56376e95ac38
Matthew Pierce <lesbianhaircut@outlook.com>
2021-08-18 22:24:06 -0400

    c

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
 window.onload = () => {
   // Variables
+
   const dropdownButton = document.querySelector(".dropdown-button");
   const dropdown = document.querySelector(".dropdown");
-  const rewardbuttons = document.querySelectorAll(".reward-button");
-  const rewardPage = document.querySelector(".reward-popup");
-  const closeRewardPageButton = document.querySelector(".close-reward-page");
+  const pledgebuttons = document.querySelectorAll(".reward-button");
+  const pledgePage = document.querySelector(".reward-popup");
+  const closePledgePageButton = document.querySelector(".close-reward-page");
+
+  // Creates list of pledge articles based on the number in their id (Ex. 1,2,3)
+  const pledgePopupArticles = [1, 2, 3].map((i) => {
+    return document.querySelector(`.reward-popup-${i}`);
+  });
+
+  // Creates list of pledge icons (green circles) based on the number in their id (Ex. 1,2,3)
+  const pledgeIcons = [1, 2, 3].map((i) => {
+    return document.querySelector(`#icon-${i}`);
+  });
 
   // Functions
 
-  function closeRewardPage() {
-    rewardPage.style.display = "none";
+  function clearPledges() {
+    // Goes through each article and removes styling
+    pledgePopupArticles.forEach((article) => {
+      article.classList.remove("active-border");
+    });
+    // Goes through each icon and removes styling
+    pledgeIcons.forEach((icon) => {
+      icon.classList.remove("active-icon");
+    });
   }
 
-  function showRewardPage(event) {
+  function highlightPledge(pledgeIndex) {
+    clearPledges(); // Calls function to remove styling from each pledge article
+    pledgePopupArticles[pledgeIndex - 1].classList.add("active-border");
+    pledgeIcons[pledgeIndex - 1].classList.add("active-icon");
+  }
+  // Close pledge-reward page when 'X' is clicked
+  function closePledgePage() {
+    pledgePage.style.display = "none";
+  }
+  // Show pledge-reward page on click
+  function showPledgePage(event) {
+    // Finds the selected pledge based on the what number it contains (Ex. 1,2,3)
     let regex = /[0-9]/g;
     let rewardIndex = event.target.id.match(regex).join("");
-    rewardPage.style.display = "block";
-    highlightPledge();
+    highlightPledge(parseInt(rewardIndex) + 1);
+    // Display the pledge-reward page
+    pledgePage.style.display = "block";
   }
 
+  // Shows or hides dropdown based on top-right hamburger click
   function toggleDropdown() {
     let img = "";
     if (dropdown.style.display == "none") {
@@ -32,11 +63,18 @@ window.onload = () => {
   }
 
   // Event Listeners
+
   dropdownButton.addEventListener("click", toggleDropdown);
 
-  rewardbuttons.forEach((button) => {
-    button.addEventListener("click", showRewardPage);
+  pledgebuttons.forEach((button) => {
+    button.addEventListener("click", showPledgePage);
   });
 
-  closeRewardPageButton.addEventListener("click", closeRewardPage);
+  closePledgePageButton.addEventListener("click", closePledgePage);
+
+  pledgeIcons.forEach((icon) => {
+    icon.parentNode.addEventListener("click", () => {
+      console.log(icon);
+    });
+  });
 };
